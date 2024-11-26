2ada003dcae0da7d58f133c7fce023feeaf31c44
jinlv <lvjin1766@gmail.com>
2024-11-26 16:50:54 +0800

    Configure Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://luoxue-star.github.io',
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
